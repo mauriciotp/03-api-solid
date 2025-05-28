af0340dd5f38c848500cc68f66b9b424f132b795
mauricio <mauricio.tp_@outlook.com>
2025-05-27 22:37:21 -0300

    feat: implement e2e tests

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -23,8 +23,6 @@ export default (<Environment>{
     const schema = randomUUID()
     const databaseUrl = generateDatabaseUrl(schema)
 
-    console.log(databaseUrl)
-
     process.env.DATABASE_URL = databaseUrl
 
     execSync('npx prisma migrate deploy')
---
+++ src/http/controllers/profile.spec.ts
@@ -0,0 +1,42 @@
+import { app } from '@/app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Profile (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to get user profile', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const authenticateResponse = await request(app.server)
+      .post('/sessions')
+      .send({
+        email: 'johndoe@example.com',
+        password: '123456',
+      })
+
+    const token = authenticateResponse.body.token
+
+    const profileResponse = await request(app.server)
+      .get('/me')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(profileResponse.statusCode).toEqual(200)
+    expect(profileResponse.body).toEqual({
+      user: expect.objectContaining({
+        name: 'John Doe',
+      }),
+    })
+  })
+})
